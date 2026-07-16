d0e2001c587e84d54a32a8a74be2c9ccfa869df3
Grazielle <grazielle.f.silva9@aluno.senai.br>
2026-07-16 15:57:39 -0300

    alteração no codigo lista_produto,alteração no index.html colocado mais uma div

--- js/lista_produto.js
+++ js/lista_produto.js
@@ -1,9 +1,9 @@
-const produtos = [
+export const produtos = [
     // ===== CELULARES =====
     {
         id_produto: 1,
-        descricao_produto: "Motorola",
-        valor_unitario: 4999.90,
+        descricao_produto: "Moto G06",
+        valor_unitario: 1700.90,
         unidade: "UN",
         caminho_imagem: "imagens/motog06.png",
         id_secao: 1,
@@ -11,7 +11,7 @@ const produtos = [
     },
     {
         id_produto: 2,
-        descricao_produto: "iPhone 16",
+        descricao_produto: "iPhone 17",
         valor_unitario: 7299.90,
         unidade: "UN",
         caminho_imagem: "/imagens/iphone17promax.png",
@@ -20,37 +20,37 @@ const produtos = [
     },
     {
         id_produto: 3,
-        descricao_produto: "Motorola Edge 50",
+        descricao_produto: "Moto G8",
         valor_unitario: 2799.90,
         unidade: "UN",
-        caminho_imagem: "img/celulares/moto-edge50.png",
+        caminho_imagem: "imagens/motorolamotogg8.png",
         id_secao: 1,
         secao: "Celulares"
     },
     {
         id_produto: 4,
-        descricao_produto: "Xiaomi Redmi Note 14",
-        valor_unitario: 1999.90,
+        descricao_produto: "Nokia 1661",
+        valor_unitario: 500.00,
         unidade: "UN",
-        caminho_imagem: "img/celulares/redmi-note14.png",
+        caminho_imagem: "imagens/nokia1661.png",
         id_secao: 1,
         secao: "Celulares"
     },
     {
         id_produto: 5,
-        descricao_produto: "Samsung Galaxy A56",
+        descricao_produto: "Samsung Galaxy",
         valor_unitario: 2499.90,
         unidade: "UN",
-        caminho_imagem: "img/celulares/galaxy-a56.png",
+        caminho_imagem: "imagens/celularsamsung.png",
         id_secao: 1,
         secao: "Celulares"
     },
     {
         id_produto: 6,
-        descricao_produto: "iPhone 15",
-        valor_unitario: 5799.90,
+        descricao_produto: "Positivo P41",
+        valor_unitario: 600.90,
         unidade: "UN",
-        caminho_imagem: "img/celulares/iphone15.png",
+        caminho_imagem: "imagens/celularpositivop41.png",
         id_secao: 1,
         secao: "Celulares"
     },
@@ -59,129 +59,7 @@ const produtos = [
         descricao_produto: "Samsung Galaxy Z Flip6",
         valor_unitario: 6999.90,
         unidade: "UN",
-        caminho_imagem: "img/celulares/zflip6.png",
+        caminho_imagem: "imagens/CelularSamsungGalaxyZFlip6.png",
         id_secao: 1,
         secao: "Celulares"
-    },
-
-    // ===== TABLETS =====
-    {
-        id_produto: 8,
-        descricao_produto: "iPad 11ª Geração",
-        valor_unitario: 3999.90,
-        unidade: "UN",
-        caminho_imagem: "img/tablets/ipad11.png",
-        id_secao: 2,
-        secao: "Tablets"
-    },
-    {
-        id_produto: 9,
-        descricao_produto: "Samsung Galaxy Tab S10",
-        valor_unitario: 4699.90,
-        unidade: "UN",
-        caminho_imagem: "img/tablets/tab-s10.png",
-        id_secao: 2,
-        secao: "Tablets"
-    },
-    {
-        id_produto: 10,
-        descricao_produto: "Xiaomi Pad 7",
-        valor_unitario: 2599.90,
-        unidade: "UN",
-        caminho_imagem: "img/tablets/xiaomi-pad7.png",
-        id_secao: 2,
-        secao: "Tablets"
-    },
-    {
-        id_produto: 11,
-        descricao_produto: "Lenovo Tab P12",
-        valor_unitario: 2299.90,
-        unidade: "UN",
-        caminho_imagem: "img/tablets/tab-p12.png",
-        id_secao: 2,
-        secao: "Tablets"
-    },
-    {
-        id_produto: 12,
-        descricao_produto: "Samsung Galaxy Tab A9+",
-        valor_unitario: 1699.90,
-        unidade: "UN",
-        caminho_imagem: "img/tablets/tab-a9plus.png",
-        id_secao: 2,
-        secao: "Tablets"
-    },
-    {
-        id_produto: 13,
-        descricao_produto: "iPad Air M3",
-        valor_unitario: 6999.90,
-        unidade: "UN",
-        caminho_imagem: "img/tablets/ipad-air-m3.png",
-        id_secao: 2,
-        secao: "Tablets"
-    },
-
-    // ===== NOTEBOOKS =====
-    {
-        id_produto: 14,
-        descricao_produto: "Acer Nitro V15",
-        valor_unitario: 4699.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/nitro-v15.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    },
-    {
-        id_produto: 15,
-        descricao_produto: "Dell Inspiron 15",
-        valor_unitario: 4199.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/inspiron15.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    },
-    {
-        id_produto: 16,
-        descricao_produto: "Lenovo IdeaPad 3",
-        valor_unitario: 3499.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/ideapad3.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    },
-    {
-        id_produto: 17,
-        descricao_produto: "ASUS VivoBook 15",
-        valor_unitario: 3899.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/vivobook15.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    },
-    {
-        id_produto: 18,
-        descricao_produto: "Samsung Galaxy Book4",
-        valor_unitario: 4399.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/galaxy-book4.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    },
-    {
-        id_produto: 19,
-        descricao_produto: "MacBook Air M3",
-        valor_unitario: 8999.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/macbook-air-m3.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    },
-    {
-        id_produto: 20,
-        descricao_produto: "Acer Aspire 5",
-        valor_unitario: 3599.90,
-        unidade: "UN",
-        caminho_imagem: "img/notebooks/aspire5.png",
-        id_secao: 3,
-        secao: "Notebooks"
-    }
-];
+    },]
